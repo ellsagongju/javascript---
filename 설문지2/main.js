520a7db640730f33728708c7c6b926444059e821
hyojin jo <mlhj6010@naver.com>
2022-10-10 17:20:56 +0900

    순서대로 나오고 순서대로 지우는것까지 성공, 재배열 하는 부분이 필요함

--- 설문지2/main.js
+++ 설문지2/main.js
@@ -7,12 +7,13 @@ $(document).ready(function () {
 let qNum = 1;
 let qCount = 0;
 let qList = [];
-
+let qNumList = [];
 
 function changeQ() {
     let qName=$('#q_list option:selected').text();
     let qCode=$('#q_list option:selected').val();
-    let qAdd="";
+	let qAdd = "";
+	
 
 		if(qCount>=10){
 			alert('질문 최대 10개까지만 선택 가능합니다.');
@@ -20,24 +21,47 @@ function changeQ() {
 		}else if(qList.includes(qCode)){
 			alert('이미 선택한 질문입니다.');
 			return false;
-		}else if(qCode.value=""){
+		}else if(qCode=""){
 			alert('질문을 선택하지 않았습니다.');
 			return false;
-		}else{
+		} else {
+			qNumList.push(qNum);
 			qList.push("qDelBtn"+qCode);
 			console.log(qList);
 			
-			qAdd='<div class="survey_list">';
+			qAdd = '<div class="survey_list">';
+			qAdd+='<span class="survey_num">';
+			qAdd+='Q'+qNum+"</span>";
 			qAdd+='<label class="survey_text">';
 			qAdd+=qName+"</label>";
-			qAdd+='<button type="button" id="qDelBtn'+qNum+'" class="qDelBtn">';
+			qAdd+='<button type="button" id="qDelBtn'+qCode+'" class="qDelBtn">';
 			qAdd+='-</button></div>';
 			
 			console.log(qAdd);
 			
-			$('#survey-list_wrap').append(qAdd);
+			$('#survey_list_wrap').append(qAdd);
 			qCount++;
 			qNum++;
+		
+			console.log(qNumList);
 		}
 
 }
+
+$(document).on('click', '.qDelBtn', function(){
+	//배열에서 빼주고 
+	let q=$(this).attr('id'); //형제의 텍스트값
+	console.log(q);
+	console.log("인덱스: " + qList.indexOf(q));
+	
+	qList.splice(qList.indexOf($(this)), 1); //배열에서 원소 제거
+	qNumList.splice(qNumList.indexOf($(this)), 1); //배열에서 원소 제거
+	console.log(qNumList);
+	
+	//리무브 해주고
+	$(this).parent().remove();
+	qCount--;
+	qNum--;
+	qNumList.filter(qList);
+	console.log(qList);
+});
