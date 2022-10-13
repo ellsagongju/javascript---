2bdf3c9b14cb7a6d14c18dad9eec894eaac5cfec
hyojin jo <mlhj6010@naver.com>
2022-10-13 23:07:26 +0900

    자바스크립트 보안부분에 대한 공부

--- 설문지2/main.js
+++ 설문지2/main.js
@@ -13,10 +13,9 @@ function changeQ() {
     let qName=$('#q_list option:selected').text();
     let qCode=$('#q_list option:selected').val();
 	let qAdd = "";
-	
 
-		if(qCount>=10){
-			alert('질문 최대 10개까지만 선택 가능합니다.');
+		if(qCount>=3){
+			alert('질문 최대 3개까지만 선택 가능합니다.');
 			return false;
 		}else if(qList.includes(qCode)){
 			alert('이미 선택한 질문입니다.');
@@ -26,15 +25,15 @@ function changeQ() {
 			return false;
 		} else {
 			qNumList.push(qNum);
-			qList.push("qDelBtn"+qCode);
-			console.log(qList);
+			let qWarp = qList.push(qCode);
+			console.log(qWarp);
 			
 			qAdd = '<div class="survey_list">';
 			qAdd+='<span class="survey_num">';
 			qAdd+='Q'+qNum+"</span>";
 			qAdd+='<label class="survey_text">';
 			qAdd+=qName+"</label>";
-			qAdd+='<button type="button" id="qDelBtn'+qCode+'" class="qDelBtn">';
+			qAdd+='<button type="button" id="qDelBtn'+qWarp+'" class="qDelBtn">';
 			qAdd+='-</button></div>';
 			
 			console.log(qAdd);
@@ -45,14 +44,13 @@ function changeQ() {
 		
 			console.log(qNumList);
 		}
-
 }
 
 $(document).on('click', '.qDelBtn', function(){
 	//배열에서 빼주고 
 	let q=$(this).attr('id'); //형제의 텍스트값
-	console.log(q);
-	console.log("인덱스: " + qList.indexOf(q));
+	// console.log(q);
+	// console.log("인덱스: " + qList.indexOf(q));
 	
 	qList.splice(qList.indexOf($(this)), 1); //배열에서 원소 제거
 	qNumList.splice(qNumList.indexOf($(this)), 1); //배열에서 원소 제거
@@ -62,6 +60,4 @@ $(document).on('click', '.qDelBtn', function(){
 	$(this).parent().remove();
 	qCount--;
 	qNum--;
-	qNumList.filter(qList);
-	console.log(qList);
 });
